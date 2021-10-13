6f1b247fec7e0e4395dd5af21699f9f77d0c9e1b
ArtemiiKh <artemii.od.ua@gmail.com>
2021-10-13 18:32:04 +0300

    Home_work_2
    Light calculator

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+//Subtraction operation
+
+let num1 = prompt('Введите первое число');
+let num2 = prompt('Введите второе число');
+
+const result = num1 - num2;
+
+console.log(result)
+
+//Summing operation
+
+// let num1 = Number(prompt('Введите первое число'));
+// let num2 = Number(prompt('Введите второе число'));
+
+// const result = num1 + num2;
+
+// console.log(result);
+
+//Multiplication operation
+
+// let num1 = Number(prompt('Введите первое число'));
+// let num2 = Number(prompt('Введите второе число'));
+
+// const result = num1 * num2;
+
+// console.log(result)
+
+// Division operation
+
+// let num1 = Number(prompt('Введите первое число'));
+// let num2 = Number(prompt('Введите второе число'));
+
+// const result = num1 / num2;
+
+// console.log(result)
